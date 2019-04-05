0c9169631bba0fe9599dba98228686efa9f6cfd8
Gianfranco <gianffh@gmail.com>
2019-04-05 09:47:11 +0700

    Modify config files

--- src/config/index.js
+++ src/config/index.js
@@ -1,29 +1,14 @@
-require('dotenv').config({ path: './.env' });
+require('dotenv').config();
 
 export default {
 	env: process.env.NODE_ENV || 'development',
-	logger: {
-		host: process.env.LOGGER_HOST, // Papertrail Logging Host
-		port: process.env.LOGGER_PORT, // Papertrail Logging Port
+	server: {
+		port: process.env.PORT,
 	},
-	email: {
-		sender: {
-			default: {
-				name: process.env.EMAIL_SENDER_DEFAULT_NAME, // Your Name
-				email: process.env.EMAIL_SENDER_DEFAULT_EMAIL, // nick@your-domain.com
-			},
-			support: {
-				name: process.env.EMAIL_SENDER_SUPPORT_NAME, // API Support
-				email: process.env.EMAIL_SENDER_SUPPORT_EMAIL, // support@your-domain.com
-			},
-		},
-		sendgrid: {
-			secret: process.env.EMAIL_SENDGRID_SECRET, // SendGrid API Secret
-		},
+	jwt: {
+		secret: process.env.JWT_SECRET,
 	},
-	stream: {
-		appId: process.env.STREAM_APP_ID, // Stream Credentials – https://getstream.io
-		apiKey: process.env.STREAM_API_KEY, // Stream Credentials – https://getstream.io
-		apiSecret: process.env.STREAM_API_SECRET, // Stream Credentials – https://getstream.io
-	},
+	database: {
+		uri: `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}`
+	}
 };
