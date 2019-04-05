91a58f2f68942f2bfddc98f87a4b65cafdeaceea
Gianfranco <gianffh@gmail.com>
2019-04-05 22:08:21 +0700

    Fix lint

--- src/controllers/method-chunk.js
+++ src/controllers/method-chunk.js
@@ -1,11 +1,6 @@
-import validator from 'validator';
-import jwt from 'jsonwebtoken';
-
 import MethodChunk from '../models/method-chunk';
 import { isMethodChunkValid } from '../utils/method-chunk';
-import logger from '../utils/logger';
 import { errorCode } from '../constants';
-import config from '../config';
 
 exports.insert = async (req, res, next) => {
   try {
