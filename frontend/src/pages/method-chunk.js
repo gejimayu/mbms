95ab31e4054e13a1d942fe6b5f8f06aa82ea8bfe
Gianfranco <gianffh@gmail.com>
2019-04-09 20:07:54 +0700

    Refactor sidebar component

--- frontend/src/pages/method-chunk.js
+++ frontend/src/pages/method-chunk.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import TreeView from '../components/treeview';
+
+import Sidebar from '../components/sidebar';
 
 export default (props) => {
   const { match } = props;
@@ -15,53 +16,10 @@ export default (props) => {
       .catch(err => alert(err))
   }, [])
 
-  const methodLabel = <span className="node">{methodChunk.name}</span>;
-  const alphasLabel = <span className="node">Alphas</span>;
-  const activitySpacesLabel = <span className="node">Activity Spaces</span>;
-  const competenciesLabel = <span className="node">Competencies</span>;
-  const patternsLabel = <span className="node">Patterns</span>;
   return (
     <Styles>
       <div className='content'>
-        {
-          methodChunk && (
-            <TreeView nodeLabel={methodLabel} defaultCollapsed={false}>
-              <TreeView key='alpha' nodeLabel={alphasLabel} defaultCollapsed={false}>
-                {methodChunk.alphas && methodChunk.alphas.map(alpha => {
-                  return <div key={alpha.nameId}  className="info">{alpha.name}</div>
-                })}
-                {methodChunk.alphas && methodChunk.alphas.map(alpha => {
-                  return <div key={alpha.nameId}  className="info">{alpha.name}</div>
-                })}
-                {methodChunk.alphas && methodChunk.alphas.map(alpha => {
-                  return <div key={alpha.nameId}  className="info">{alpha.name}</div>
-                })}
-              </TreeView>
-              <TreeView key='activity-space' nodeLabel={activitySpacesLabel} defaultCollapsed={false}>
-                {methodChunk.activitySpaces && methodChunk.activitySpaces.map(activitySpace => {
-                  const activitySpaceLabel = <span className="node">{activitySpace.name}</span>;
-                  return (
-                    <TreeView key={activitySpace.nameId} nodeLabel={activitySpaceLabel} defaultCollapsed={false}>
-                      {activitySpace.activities.map(activity => {
-                        return <div key={activity.nameId} className="info">{activity.name}</div>
-                      })}
-                    </TreeView>
-                  );
-                })}
-              </TreeView>
-              <TreeView key='competency' nodeLabel={competenciesLabel} defaultCollapsed={false}>
-                {methodChunk.competencies && methodChunk.competencies.map(competency => (
-                  <div key={competency.nameId} className="info">{competency.name}</div>
-                ))}
-              </TreeView>
-              <TreeView key='pattern' nodeLabel={patternsLabel} defaultCollapsed={false}>
-                {methodChunk.patterns && methodChunk.patterns.map(pattern => (
-                  <div key={pattern.nameId} className="info">{pattern.name}</div>
-                ))}
-              </TreeView>
-            </TreeView>
-          )
-        }
+        { methodChunk && <Sidebar methodChunk={methodChunk}/> }
       </div>
     </Styles>
   )
@@ -71,23 +29,5 @@ const Styles = styled.div`
   .content {
     display: flex;
     flex-direction: row;
-    & > .tree-view {
-      height: 75vh;
-      font-size: 0.8em;
-      padding: 0 16px;
-      border-right: 1px solid black;
-      position: fixed;
-      overflow-y: scroll;
-      z-index: 1;
-      ::-webkit-scrollbar {
-        -webkit-appearance: none;
-        width: 10px;
-      }
-      ::-webkit-scrollbar-thumb {
-        border-radius: 5px;
-        background-color: rgba(0,0,0,.5);
-        -webkit-box-shadow: 0 0 1px rgba(255,255,255,.5);
-      }
-    }
   }
 `
