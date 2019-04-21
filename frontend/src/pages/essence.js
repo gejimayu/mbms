4a92f95a1042bcd7bb2b196bf956d8571f52e112
Gianfranco <gianffh@gmail.com>
2019-04-21 15:35:04 +0700

    Use react lazy for routing in essence page

--- frontend/src/pages/essence.js
+++ frontend/src/pages/essence.js
@@ -3,11 +3,11 @@ import { Route, Switch } from 'react-router-dom';
 
 import './essence.scss'
 import Sidebar from '../components/sidebar';
-import MethodChunk from './methodchunk';
-import Alpha from './alpha';
-import Activity from './activity';
-import Competency from './competency';
-import Pattern from './pattern';
+const MethodChunk = React.lazy(() => import('./methodchunk'));
+const Alpha = React.lazy(() => import('./alpha'));
+const Activity = React.lazy(() => import('./activity'));
+const Competency = React.lazy(() => import('./competency'));
+const Pattern = React.lazy(() => import('./pattern'));
 
 export default (props) => {
   const { match } = props;
@@ -26,28 +26,30 @@ export default (props) => {
     return (
       <div className='main'>
         <Sidebar className='sidebar' methodChunk={methodChunk} />
-        <Switch>
-          <Route
-            path={`${match.path}/alpha/:name_id`}
-            render={props => <Alpha {...props} methodChunk={methodChunk} />}
-          />
-          <Route
-            path={`${match.path}/activity/:name_id`}
-            render={props => <Activity {...props} methodChunk={methodChunk} />}
-          />
-          <Route
-            path={`${match.path}/competency/:name_id`}
-            render={props => <Competency {...props} methodChunk={methodChunk} />}
-          />
-          <Route
-            path={`${match.path}/pattern/:name_id`}
-            render={props => <Pattern {...props} methodChunk={methodChunk} />}
-          />
-          <Route
-            path={`${match.path}`}
-            render={props => <MethodChunk {...props} methodChunk={methodChunk} />}
-          />
-        </Switch>
+        <React.Suspense fallback={<p>Please Wait</p>}>
+          <Switch>
+            <Route
+              path={`${match.path}/alpha/:name_id`}
+              render={props => <Alpha {...props} methodChunk={methodChunk} />}
+            />
+            <Route
+              path={`${match.path}/activity/:name_id`}
+              render={props => <Activity {...props} methodChunk={methodChunk} />}
+            />
+            <Route
+              path={`${match.path}/competency/:name_id`}
+              render={props => <Competency {...props} methodChunk={methodChunk} />}
+            />
+            <Route
+              path={`${match.path}/pattern/:name_id`}
+              render={props => <Pattern {...props} methodChunk={methodChunk} />}
+            />
+            <Route
+              path={`${match.path}`}
+              render={props => <MethodChunk {...props} methodChunk={methodChunk} />}
+            />
+          </Switch>
+        </React.Suspense>
       </div>
     )
   }
